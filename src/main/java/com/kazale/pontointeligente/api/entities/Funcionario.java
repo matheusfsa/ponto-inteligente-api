4d22987f8a2bbd4f5be5502c64a151efc515b8cb
Matheus <tetofsa15@gmail.com>
2018-09-20 01:45:37 -0300

    entities criadas

---
+++ src/main/java/com/kazale/pontointeligente/api/entities/Funcionario.java
@@ -0,0 +1,12 @@
+package com.kazale.pontointeligente.api.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "funcionario")
+public class Funcionario implements Serializable{
+	
+}

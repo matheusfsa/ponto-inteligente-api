488f4e56515b46f3581d3fa1abefca37360eadd9
Matheus <tetofsa15@gmail.com>
2018-09-21 10:47:25 -0300

    Aula 18

---
+++ src/test/java/com/kazale/pontointeligente/api/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.kazale.pontointeligente.api.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }
+}
